e05817170cf7aebc5a36c1133fc6dd57be9fb4fc
unknown <burkeamy30@gmail.com>
2021-06-02 21:36:35 -0400

    added a custom svg logo and some other stuff, i think

--- src/components/top-intro/top-intro.component.jsx
+++ src/components/top-intro/top-intro.component.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './top-intro.styles.css';
-import { Link } from 'react-router-dom';
+/*import { Link } from 'react-router-dom';*/
 import MenuIcon from '../menuIcon/menuIcon.component';
-import SvgComponent from '../Icon/icon.component';
+import Icon from '../Icon/icon.component';
 
 
 const TopIntro = () => {
 
     return (
         <div className = 'top-intro'>
-            <SvgComponent />
+            <Icon />
             <h1>Alyssa's Garden, LLC</h1>
             <MenuIcon />
+            <div className='tag-line-area'>
+                <p className="tag-line">Don't sweat the small stuff, that's our job</p>
+                <p className="tag-line" >Detailed garden specalist</p>
+            </div>
         </div>
     )
 };
