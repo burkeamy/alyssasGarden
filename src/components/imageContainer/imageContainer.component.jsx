7bf0d5a47cec8324123a78e937293ff3f9f55758
unknown <burkeamy30@gmail.com>
2021-06-13 20:59:49 -0400

    added a couple photos to the portfolio

--- src/components/imageContainer/imageContainer.component.jsx
+++ src/components/imageContainer/imageContainer.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './imageContainer.styles.css';
 import Mulch from '../../images/lotsOfMulch.png';
 import SmallPerennialGarden from '../../images/smallPerennialGarden.png';
+import SageAndBasil from '../../images/sageAndBasil.png';
+import ButterflyPot from '../../images/butterflyPot.png';
 
 const ImageContainer = () => {
 
@@ -9,6 +11,8 @@ const ImageContainer = () => {
         <div className = 'image-container'>
             <img className='image-display'alt ='four yards of triple shred mulch in a flower bed'src= { Mulch }></img>
             <img className='image-display'alt ='a small perennial garden'src= { SmallPerennialGarden }></img>
+            <img className='image-display'alt ='a pot filled with sage, basil, and tomato'src= { SageAndBasil }></img>
+            <img className='image-display'alt ='a painted flower pot filled with flowers'src= { ButterflyPot }></img>
         </div>
     )
 };
