0f537926c121aa914b5c016cf50eea85a4c6ada7
unknown <burkeamy30@gmail.com>
2021-06-13 20:25:01 -0400

    added working contat form

--- src/pages/contact/contactPage.component.jsx
+++ src/pages/contact/contactPage.component.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 import ThankYou from '../../components/ThankYou/thankYou.component';
 import './contactPage.styles.css';
-//import emailjs from 'emailjs-com';
+import emailjs from 'emailjs-com';
 
 
 const ContactPage = () => {
@@ -14,12 +14,12 @@ const ContactPage = () => {
         setIsSubmitted(true);
                
     
-            /*emailjs.sendForm('service_os18hzg', 'template_hulbr91', e.target, 'user_SVeso4VCm5a8LFlI5j2Tf')
+            emailjs.sendForm('service_os18hzg', 'template_zvwxdir', e.target, 'user_SVeso4VCm5a8LFlI5j2Tf')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
             console.log(error.text);
-        });*/
+        });
             
     }
     
@@ -33,11 +33,11 @@ const ContactPage = () => {
                      <h3>Please fill out this short form for additional information aboout our services</h3>
                      <form className="contact-form" onSubmit={sendEmail}>
                        <label>Your name</label>
-                       <input type="text" name="name" placeholder="Your name" className="enter-name"/>
+                       <input type="text" name="name" placeholder="Your name" className="enter-name" required/>
                        <label>Email address</label>
-                       <input type="email" name="email"placeholder="Your email" className="enter-email"/>
+                       <input type="email" name="email"placeholder="Your email" className="enter-email" required/>
                        <label>What kind of services are you looking for.</label>
-                       <textarea name="message" placeholder="A brief message about what you are intrested in" className="enter-message"/>
+                       <textarea name="message" placeholder="A brief message about what you are intrested in" className="enter-message" required/>
                        <br />
                        <input type="submit" value="Send" className="submit-button"/>
                      </form>
