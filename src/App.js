e881fb606a9687bceeadab760bec1d9ee6580276
unknown <burkeamy30@gmail.com>
2021-08-01 15:53:04 -0400

    deleted some commented out code

--- src/App.js
+++ src/App.js
@@ -13,11 +13,8 @@ import ReactGA from 'react-ga';
 
 import { withRouter } from 'react-router-dom';
 
-//export const initGA = () => {       
+ 
   ReactGA.initialize('UA-202651038-1'); 
-  //ReactGA.pageview(window.location.pathname + window.location.search);
-//} 
-
 
 function App() {
 
