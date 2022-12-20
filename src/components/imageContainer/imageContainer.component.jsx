d10e625e4395ce254b47d02e1b14c590bd91bcdd
Amy Burke <burkeamy30@gmail.com>
2022-12-20 17:55:19 -0500

    added pictures to portfolio page

--- src/components/imageContainer/imageContainer.component.jsx
+++ src/components/imageContainer/imageContainer.component.jsx
@@ -4,6 +4,7 @@ import Mulch from '../../images/lotsOfMulch.png';
 import SmallPerennialGarden from '../../images/smallPerennialGarden.png';
 import SageAndBasil from '../../images/sageAndBasil.png';
 import ButterflyPot from '../../images/butterflyPot.png';
+import Windowbox from '../../images/windowbox.png';
 
 const ImageContainer = () => {
 
@@ -12,6 +13,7 @@ const ImageContainer = () => {
             <img className='image-display'alt ='four yards of triple shred mulch in a flower bed'src= { Mulch }></img>
             <img className='image-display'alt ='a small perennial garden'src= { SmallPerennialGarden }></img>
             <img className='image-display'alt ='a pot filled with sage, basil, and tomato'src= { SageAndBasil }></img>
+            <img className='image-display'alt ='a window box full of scullents'src= { Windowbox }></img>
             <img className='image-display'alt ='a painted flower pot filled with flowers'src= { ButterflyPot }></img>
         </div>
     )
