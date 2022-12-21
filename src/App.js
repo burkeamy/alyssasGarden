9701e279d09a59f742f5fc0b8251fcfa53dcc76b
Amy Burke <burkeamy30@gmail.com>
2022-12-21 11:00:04 -0500

    adding a services page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ContactPage from './pages/contact/contactPage.component';
 import TopIntro from './components/top-intro/top-intro.component';
 import NavArea from './components/nav-area/nav-area.component';
 import Portfolio from './pages/portfolio/portfolio.component';
+import Services from './pages/services/services.component';
 import Footer from './components/footer/footer.component';
 import ReactGA from 'react-ga';
 
@@ -29,6 +30,7 @@ function App() {
       <NavArea />
       <Switch>
         <Route exact path='/' component={ HomePage }/>
+        <Route exact path='/services' component={ Services } />
         <Route exact path='/about-us' component={ AboutUs } />
         <Route exact path='/portfolio' component={ Portfolio } />
         <Route exact path='/contact-us' component={ ContactPage } />
---
+++ src/pages/services/services.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './services.styles.css';
+
+
+
+const Services = () => (
+    <div className ='services'>
+        <article className='services-usContainer'>
+            <h2>Services</h2>
+            <p className='open-p'>
+                we offer a variety of services including
+                </p>
+                    <ul>
+                        <li>Landscape maintenance</li>
+                        <li>summer and winter annuals</li>
+                        <li>container planting</li>
+                        <li>Mulching</li>
+                        <li>leaf management</li>
+                    </ul>
+                <p> 
+            </p>
+        </article>
+    </div>
+);
+
+export default Services;
